8d9995528f5dbef2d9b139849d30e471d1d58920
Maxime Lacroix <maxime.lacroix@epitech.eu>
2017-07-13 10:24:30 +0200

    oops bon jarrete de push toutes les deux secondes

--- cli/main.c
+++ cli/main.c
@@ -5,13 +5,15 @@
 ** Login   <lacroi_m@epitech.net>
 ** 
 ** Started on  Thu Jul 13 08:06:13 2017 Maxime Lacroix
-** Last update Thu Jul 13 10:21:50 2017 Maxime Lacroix
+** Last update Thu Jul 13 10:24:01 2017 Maxime Lacroix
 */
 
 #include "cli.h"
 
 int main(int ac, char **av)
 {
-  // if ()
+  (void)av;
+  if (ac < 5 || ac > 5)
+    return (84);
   return (0);
 }
