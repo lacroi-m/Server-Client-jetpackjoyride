9a035c8f63db5bcb6362eded59b9570a6be55a99
Maxime Lacroix <maxime.lacroix@epitech.eu>
2017-07-13 10:22:07 +0200

    adding map

--- cli/main.c
+++ cli/main.c
@@ -5,14 +5,13 @@
 ** Login   <lacroi_m@epitech.net>
 ** 
 ** Started on  Thu Jul 13 08:06:13 2017 Maxime Lacroix
-** Last update Thu Jul 13 09:56:37 2017 Maxime Lacroix
+** Last update Thu Jul 13 10:21:50 2017 Maxime Lacroix
 */
 
 #include "cli.h"
 
 int main(int ac, char **av)
 {
-  (void)ac;
-  (void)av;
+  // if ()
   return (0);
 }
